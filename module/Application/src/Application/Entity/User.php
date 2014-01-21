047c1dc4fc3abf57623e911cb1881438193bbd56
shivakumardesai <shivakumardesai@gmail.com>
2014-01-21 23:16:50 +0530

    initial commit

---
+++ module/Application/src/Application/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Application\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/** @ORM\Entity */
+class User {
+    /**
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    * @ORM\Column(type="integer")
+    */
+    protected $id;
+
+    /** @ORM\Column(type="string") */
+    protected $fullName;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName($value)
+    {
+        $this->fullName = $value;
+    }
+}
